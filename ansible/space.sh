11547788415e97b7bf42725fe844f3cb18fd362c
Ubuntu <ubuntu@ip-172-31-57-187.ec2.internal>
2020-07-27 15:57:40 +0000

    uploaded new code for tech test

---
+++ ansible/space.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+#
+# removal of files over 7 days old if the filesystem is over 90%
+#
+dir="/home/ubuntu/bede_tech/test"
+days=7
+
+df -H | grep -vE '^Filesystem|tmpfs|cdrom|loop0|loop1|udev' | awk '{ print $5 " " $1 }' | while read output;
+do
+  echo $output
+  usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
+  partition=$(echo $output | awk '{ print $2 }' )
+  if [ $usep -ge 90 ]; then
+    echo -n "removing files in $dir that are older than $days days: "
+    find "$dir" -mtime +$days -type f -exec echo {} \; -exec rm {} \; | wc -l
+  fi
+done
