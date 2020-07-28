2d3dd8518e416f968f984e16fd338c1fb86dc604
Ubuntu <ubuntu@ip-172-31-57-187.ec2.internal>
2020-07-28 13:48:09 +0000

    Code changes

--- scripts/space.sh
+++ scripts/space.sh
@@ -10,7 +10,7 @@ do
   echo $output
   usep=$(echo $output | awk '{ print $1}' | cut -d'%' -f1  )
   partition=$(echo $output | awk '{ print $2 }' )
-  if [ $usep -ge 20 ]; then
+  if [ $usep -ge 90 ]; then
     echo -n "removing files in $dir that are older than $days days: "
     find "$dir" -mtime +$days -type f -exec echo {} \; -exec rm {} \; | wc -l
   fi
